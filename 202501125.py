11b634eee49ef186ae8cb4b7ef0e0398a7ca12b0
liambamufaya <196014763+liambamufaya@users.noreply.github.com.>
2026-04-15 10:15:17 +0200

    Include both fibonacci and factorial functions

--- 202501125.py
+++ 202501125.py
@@ -3,8 +3,26 @@ def Factorial(x):
         return 1
     else:
         return x * Factorial(x-1)
-
-
+    
+def Fibonacci(x):
+     if x ==1:
+          return 1
+     elif x ==0:
+          return 0
+     else:
+          return Fibonacci(x-1) + Fibonacci(x-2)
+     
 #Get input from the user
 number = int(input("Enter number "))
-print(Factorial(number))
+
+if number <=0 :
+    print("Enter positive integer")
+else:
+        print("Factorial of",number,"is ")
+        print(Factorial(number))
+        print()
+        print("Fibonacci number of",number, "is")
+        
+        for i in range(number):
+            print(Fibonacci(i), end=" ")
+print()
