a56461174f398fb7d611b7a69ac2d030cd31eb35
liambamufaya <196014763+liambamufaya@users.noreply.github.com.>
2026-04-15 09:48:00 +0200

    Get input from user

--- 202501125.py
+++ 202501125.py
@@ -3,7 +3,8 @@ def Factorial(x):
         return 1
     else:
         return x * Factorial(x-1)
-    
 
 
-print(Factorial(10)) 
+#Get input from the user
+number = int(input("Enter number "))
+print(Factorial(number))
